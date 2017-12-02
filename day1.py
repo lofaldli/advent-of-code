5f4cdff66eb3ce1d7e1493a4d5b741db79dd6467
lofaldli <andre.lofaldli@gmail.com>
2017-12-02 09:21:49 +0100

    golfed day 1

--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 
 def solve(digits, n):
@@ -7,6 +8,14 @@ def solve(digits, n):
             sum += digits[i]
     return sum
 
+def golfed(digits, n):
+    return sum(x for x, y in zip(digits, digits[n:]+digits[:n]) if x == y)
+
+
 digits = list(map(int, sys.argv[1]))
 print('part 1:', solve(digits, 1))
 print('part 2:', solve(digits, len(digits)//2))
+
+print('golfed')
+print('part 1:', golfed(digits, 1))
+print('part 2:', golfed(digits, len(digits)//2))
