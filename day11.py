28b81b5f7ea4cd1057638dc436e4d576ae913c69
lofaldli <andre.lofaldli@gmail.com>
2017-12-11 07:43:17 +0100

    day 11

---
+++ day11.py
@@ -0,0 +1,19 @@
+from aocd import data
+
+# https://www.redblobgames.com/grids/hexagons/#neighbors-cube
+DIRS = {         'n': (0, 1,-1),
+    'nw': (-1, 1,0),          'ne': ( 1,0,-1),
+    'se': ( 1,-1,0),          'sw': (-1,0, 1),
+                 's': (0,-1, 1)}
+
+def dist(a, b=(0,0,0)):
+    return max(abs(x-y) for x,y in zip(a,b))
+
+pos = (0,0,0)
+max_dist = 0
+for dir in data.split(','):
+    pos = tuple(map(sum, zip(pos, DIRS[dir])))
+    max_dist = max(max_dist, dist(pos))
+
+print('part 1:', dist(pos))
+print('part 2:', max_dist)
