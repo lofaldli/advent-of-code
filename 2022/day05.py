bde6826b3157676c814978623194b3e2eeb991db
André Løfaldli <andre@lofaldli.com>
2022-12-05 21:44:52 +0100

    Add visualization

--- 2022/day05.py
+++ 2022/day05.py
@@ -1,3 +1,4 @@
+from time import sleep
 from aocd import data
 
 def parse_stacks(lines):
@@ -22,14 +23,32 @@ def execute9000(steps, stacks):
     for n, src, dst in steps:
         for _ in range(n):
             stacks[dst].append(stacks[src].pop())
+            print(dump(stacks))
+            sleep(0.01)
 
 def execute9001(steps, stacks):
     for n, src, dst in steps:
         stacks[dst].extend(stacks[src][-n:])
         stacks[src] = stacks[src][:-n]
+        print(dump(stacks))
+        sleep(0.01)
 
 def top_crates(stacks):
     return ''.join(s[-1] for s in stacks.values())
+
+def dump(stacks):
+    lines = [' '.join(f' {s} ' for s in stacks.keys())]
+    max_height = sum(len(stack) for stack in stacks.values()) 
+
+    for i in range(max_height):
+        chunks = []
+        for stack in stacks.values():
+            if i < len(stack):
+                chunks.append(f'[{stack[i]}]')
+            else:
+                chunks.append('   ')
+        lines.append(' '.join(chunks))
+    return '\n'.join(reversed(lines))
 
 stackinput, stepinput = data.split('\n\n')
 steps = parse_steps(stepinput.splitlines())
@@ -37,6 +56,7 @@ steps = parse_steps(stepinput.splitlines())
 stacks = parse_stacks(stackinput.splitlines())
 execute9000(steps, stacks)
 print('part 1', top_crates(stacks))
+sleep(3)
 
 stacks = parse_stacks(stackinput.splitlines())
 execute9001(steps, stacks)
